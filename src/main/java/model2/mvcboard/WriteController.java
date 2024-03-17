63456639e60c0d83c512bee7265b5f23051c8d7f
juri <ojl98@naver.com>
2024-03-17 15:28:46 +0900

    WriteController의 doPost() 작성

--- src/main/java/model2/mvcboard/WriteController.java
+++ src/main/java/model2/mvcboard/WriteController.java
@@ -45,8 +45,24 @@ public class WriteController extends HttpServlet{
 		
 		//원본 파일명과 저장된 파일 이름 설정
 		if(originalFileName != "") {
+			//첨부 파일이 있을 경우 파일명 변경
 			String savedFileName = FileUtil.renameFile(saveDirectory, originalFileName);
+			dto.setOfile(originalFileName); //원래 파일 이름
+			dto.setSfile(savedFileName); // 서버에 저장된 파일 이름
 		}
+		
+		MVCBoardDAO dao = new MVCBoardDAO();
+		int result = dao.insertWrite(dto);
+		dao.close();
+		
+		if(result == 1) {
+			//글쓰기 성공했으면 list로 이동
+			resp.sendRedirect("../mvcboard/list.do");
+		}else {
+			//글쓰기 실패했으면 에러메시지 출력
+			JSFunction.alertLocation(resp, "글쓰기에 실패했습니다.", "../mvcboard/write.do");
+		}
+		
 	}
 
 	
