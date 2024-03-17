daf78a7ca3118454d446e6ba99a9752077e4387e
juri <ojl98@naver.com>
2024-03-17 15:30:47 +0900

    JSFunction에 alert후 뒤로가기하는 alertBack() 추가

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -10,13 +10,33 @@ public class JSFunction {
 		try {
 			resp.setContentType("text/html;charset=UTF-8");
 			/*
- 				전 예제에선 jsp에서 함수를 불러와서 script를 동적 출력하기 위해 JspWriter를 쎃는데
+ 				전 예제에선 jsp에서 함수를 불러와서 script를 동적 출력하기 위해 JspWriter를 썼는데
  				이 예제에선 서블릿에서 함수를 불러오므로 PrintWriter를 사용하여 script를 출력한다.
 			*/
 			PrintWriter writer = resp.getWriter();
 			String script = "<script>"
 						  + "	alert('" + msg + "');"
 						  + "	location.href='" + url + "';"
+						  + "</script>";
+			writer.print(script);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	//alert를 띄우고 뒤로가기
+	public static void alertBack(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			/*
+ 				전 예제에선 jsp에서 함수를 불러와서 script를 동적 출력하기 위해 JspWriter를 썼는데
+ 				이 예제에선 서블릿에서 함수를 불러오므로 PrintWriter를 사용하여 script를 출력한다.
+			*/
+			PrintWriter writer = resp.getWriter();
+			String script = "<script>"
+						  + "	alert('" + msg + "');"
+						  + "	history.back();"
 						  + "</script>";
 			writer.print(script);
 		} catch (Exception e) {
