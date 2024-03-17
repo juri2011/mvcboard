d7d0dac6a4cb88f4a1993d6738be2b2faaae831c
juri <ojl98@naver.com>
2024-03-17 15:16:27 +0900

    예외처리시 alert 출력을 위한 JSFunction 클래스 생성

--- src/main/java/model2/mvcboard/WriteController.java
+++ src/main/java/model2/mvcboard/WriteController.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import utils.FileUtil;
+import utils.JSFunction;
+
 public class WriteController extends HttpServlet{
 
 	private static final long serialVersionUID = 1L;
@@ -25,8 +28,25 @@ public class WriteController extends HttpServlet{
 		
 		//파일 업로드
 		String originalFileName = "";
+		try {
+			originalFileName = FileUtil.uploadFile(req, saveDirectory);
+		} catch (Exception e) {
+			JSFunction.alertLocation(resp, "파일 업로드 오류입니다.", "../mvcboard/write.do");
+			return;
+		}
 		
 		//2. 파일 업로드 외 처리 ==========================
+		//form값을 DTO에 저장
+		MVCBoardDTO dto = new MVCBoardDTO();
+		dto.setName(req.getParameter("name"));
+		dto.setName(req.getParameter("title"));
+		dto.setName(req.getParameter("content"));
+		dto.setName(req.getParameter("pass"));
+		
+		//원본 파일명과 저장된 파일 이름 설정
+		if(originalFileName != "") {
+			String savedFileName = FileUtil.renameFile(saveDirectory, originalFileName);
+		}
 	}
 
 	
---
+++ src/main/java/utils/JSFunction.java
@@ -0,0 +1,27 @@
+package utils;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class JSFunction {
+	//alert를 띄우고 지정한 경로로 이동
+	public static void alertLocation(HttpServletResponse resp, String msg, String url) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			/*
+ 				전 예제에선 jsp에서 함수를 불러와서 script를 동적 출력하기 위해 JspWriter를 쎃는데
+ 				이 예제에선 서블릿에서 함수를 불러오므로 PrintWriter를 사용하여 script를 출력한다.
+			*/
+			PrintWriter writer = resp.getWriter();
+			String script = "<script>"
+						  + "	alert('" + msg + "');"
+						  + "	location.href='" + url + "';"
+						  + "</script>";
+			writer.print(script);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
