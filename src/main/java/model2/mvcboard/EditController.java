ab0df5e4f554a49ee3bcc2efa2c98152aa61f8d5
juri <ojl98@naver.com>
2024-03-17 19:15:59 +0900

    EditController 작성

---
+++ src/main/java/model2/mvcboard/EditController.java
@@ -0,0 +1,30 @@
+package model2.mvcboard;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/edit.do")
+@MultipartConfig(
+	maxFileSize = 1024 * 1024 * 1,
+	maxRequestSize = 1024 * 1024 * 10
+)
+public class EditController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String idx = req.getParameter("idx");
+		MVCBoardDAO dao = new MVCBoardDAO();
+		MVCBoardDTO dto = dao.selectView(idx);
+		req.setAttribute("dto", dto);
+		req.getRequestDispatcher("/Edit.jsp").forward(req, resp);
+	}
+	
+	
+}
