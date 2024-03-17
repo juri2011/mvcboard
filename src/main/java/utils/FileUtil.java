3db61892ae443c6a5bc181f7808ea9e0e38db7af
juri <ojl98@naver.com>
2024-03-17 17:57:04 +0900

    FileUtil에 파일 삭제하는 deleteFile() 작성

--- src/main/java/utils/FileUtil.java
+++ src/main/java/utils/FileUtil.java
@@ -94,4 +94,12 @@ public class FileUtil {
 				e.printStackTrace();
 			}
 	}
+	
+	public static void deleteFile(HttpServletRequest req, String directory, String filename) {
+		String sDirectory = req.getServletContext().getRealPath(directory);
+		File file = new File(sDirectory + File.separator + filename);
+		if(file.exists()) {
+			file.delete();
+		}
+	}
 }
